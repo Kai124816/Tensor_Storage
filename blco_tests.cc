2d006e6990b8222f596cc85ab373841be92f9f56
Kai Hogan <khogan@ix-dev.cs.uoregon.edu>
2025-08-04 01:02:25 -0700

    added test file

---
+++ blco_tests.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <random>
+#include <unordered_set>
+#include <tuple>
+#include <stdexcept>
+#include <type_traits>
+#include <limits>
+#include "utils.h"
+#include "tensor_impl.h"   
+#include "alto_impl.h"   
+#include "blco_impl.h"
+
+
+void test_large_blco_tensor()
+{
+    std::cout << "Testing large BLCO tensor\n";
+    std::cout << "\n";
+
+    const int R = 40000000, C = 85000000, D = 10000000;
+
+    float freq = 0.000000000000000000003f;
+
+    std::vector<NNZ_Entry<int>> test_vec = generate_block_sparse_tensor(R,C,D,freq,0,100);
+
+    std::cout<<"Entries:"<<"\n";
+    print_entry_vec(test_vec);
+    std::cout<<"\n";
+
+    BLCO_Tensor_3D<int,__uint128_t> blco(test_vec, R, C, D);
+
+    int bits_printed = 64;
+
+    std::cout << "BLCO bitmasks:\n";
+    for (const auto& a : blco.get_modemasks()) {
+        print_lsb_bits(a,bits_printed);
+    }
+    std::cout << "\n";
+
+    const std::pair<std::vector<uint64_t>, std::vector<int>> blco_indexes = blco.get_blco();
+
+    std::cout << "BLCO entries:\n";
+    for (int i = 0; i < blco_indexes.first.size(); i++) {
+        std::cout << "Block: " << blco.find_block(i)<< "\n" ;
+        std::cout << "Linear Index: ";  
+        print_uint64(blco_indexes.first[i],bits_printed);
+        std::cout << "Value: " << blco_indexes.second[i] << "\n";
+        std::cout << "\n";
+    }
+    std::cout << "\n";
+
+    for(int i = 0; i<test_vec.size(); i++){
+        int row_ind = blco.get_mode_idx_blco(blco_indexes.first[i],i,1);
+        int col_ind = blco.get_mode_idx_blco(blco_indexes.first[i],i,2);
+        int depth_ind = blco.get_mode_idx_blco(blco_indexes.first[i],i,3);
+        int val = blco_indexes.second[i];
+        find_entry(test_vec, row_ind, col_ind, depth_ind, val);
+    }
+
+    std::cout << "\n";
+
+    std::cout << "Tests finished!\n";
+}
+
+int main() {
+    test_large_blco_tensor();
+    return 0;
+};
